d2cc0dc5aafe533f8bf0164173e44d31460ab3f2
TYKUHN2 <tyler.h.kuhn@gmail.com>
2018-05-22 21:40:56 -0400

    A SCHEDULER Implemented an extremely basic scheduler that only works for the primary processor Whilst this scheduler is overly basic it allows immediate testing of existing code before expanding the emulator Also added a primary monitor for the time being until the emulator supports specifing hardware

--- entry.cpp
+++ entry.cpp
@@ -1,11 +1,28 @@
 #include "Processor.h" //Include processor type to construct initial device
 #include "Executable.h" //Include executable type to load inital ROM
 #include "Result.h" //Include emulator result codes
+#include <thread>
+#include <string>
+#include "LEM1802.h"
 
 #define DEFAULT_CLOCK 1000 //Set clock speed for default processor in Hz
 
 int main(int argc, char* args)
 {
+	float clock = DEFAULT_CLOCK;
+	
+	if (argc > 1)
+	{
+		try
+		{
+			std::string input{ args[1] };
+			clock *= std::stof(input);
+		}
+		catch (std::exception e)
+		{
+		}
+	}
+
 	Executable rom{ "./rom.dcpu" };
 
 	char * romData = nullptr;
@@ -24,7 +41,17 @@ int main(int argc, char* args)
 
 	Processor main{ romData };
 
+	LEM1802 monitor{ &main };
+
 	delete romData;
+
+	std::chrono::duration<float, std::ratio<1>> period{ 1 / clock };
+
+	while (true)
+	{
+		main.process();
+		std::this_thread::sleep_for(period);
+	}
 
 	return SUCCESS;
 }
