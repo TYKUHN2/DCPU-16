e1cbe7c102d954918d658dcb880ed1079e17f914
Tyler Kuhn <tyler.h.kuhn@gmail.com>
2019-05-05 21:21:50 -0400

    Organized Repository
    Organized the repository into folders matching the local filters I used.

--- Display/Monitor.h
+++ Display/Monitor.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include "Ticking.h"
 
 typedef unsigned int uint;
 
 class Monitor : public Ticking {
-protected:
 	sf::RenderWindow window;
+protected:
+	sf::RenderTexture target;
 
 public:
 	void tick() override;
--- Wireless.cpp
+++
@@ -1,8 +0,0 @@
-#include "Wireless.h"
-#include <vector>
-
-std::vector<Wireless *> universe;
-
-Wireless::Wireless(Processor * proc, Vendors vendor, HWSubclass subclass, HWAPI api, uint16_t id, uint16_t vers) : Peripheral(proc, vendor, HWType{ HWClass{HWClass::WIRELESS, (int)subclass}, api, id }, vers) {
-	universe.push_back(this);
-}
