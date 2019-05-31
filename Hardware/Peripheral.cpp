ff9c3ebd18303d51745bb9923033e524e46c9d2b
Tyler Kuhn <tyler.h.kuhn@gmail.com>
2019-05-31 01:16:07 -0400

    Fixed Hardware Information Calculation
    Fixed how hardware type information is calculated to bypass undefined behavior with bit fields and erroneous/excessive byte order swapping.

--- Hardware/Peripheral.cpp
+++ Hardware/Peripheral.cpp
@@ -8,11 +8,10 @@ Peripheral::Peripheral(Processor * target, Vendors manufacturer, HWType type, ui
 		type.hwclass = HWClass{ type.hwclass.subclass, type.hwclass.hwclass }; //Fix endianness
 
 	uint8_t rawclass = *(uint8_t*)& type.hwclass;
-	uint16_t rawapi = *(uint16_t*)& type.api;
+
+	uint8_t rawapi = (type.api.id << 4) | (type.api.extended ? 0b1000 : 0b0000) | (type.api.memory ? 0b0100 : 0b0000) | (type.api.reset ? 0b0010 : 0b0000) | (type.api.interrupts ? 0b0001 : 0b0000);
 
 	uint32_t raw = rawclass << 24 | rawapi << 16 | type.id;
-
-	swapBO((char*)&raw, 4, 1);
 
 	this->type = raw;
 
